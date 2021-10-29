9a9e87b25b611588f015a316bc25a1cc9400db13
juaamol <jambulud@gmail.com>
2021-10-29 20:30:58 +0200

    fix: #6. MovieEto nested inside movie response

--- src/app/single-movie/services/movie/movie.service.ts
+++ src/app/single-movie/services/movie/movie.service.ts
@@ -4,25 +4,27 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { api } from 'src/environments/environment';
 import { Movie } from '../../models/movie';
-import { MovieEto } from '../../models/movie-eto';
+import { MovieResponse } from '../../models/movie-response';
 import { etoToMovie } from '../../utils/utils';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovieService {
-
   constructor(private httpClient: HttpClient) {}
 
   /**
    * Gets the details of a movie
-   * 
+   *
    * @param movieId the id of the movie to be retrieved
    */
   getMovieDetails(movieId: string): Observable<Movie> {
     return this.httpClient
-      .get<MovieEto>(this.getMovieDetailsUrl(movieId))
-      .pipe(map((movieEto) => etoToMovie(movieEto)));
+      .get<MovieResponse>(this.getMovieDetailsUrl(movieId))
+      .pipe(
+        map((response) => response.results),
+        map((movieEto) => etoToMovie(movieEto))
+      );
   }
 
   private getMovieDetailsUrl(movieId: string): string {
