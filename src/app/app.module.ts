86e1bb53e42250f11db99e6b7bcbe2f0d84eadaf
juaamol <jambulud@gmail.com>
2021-10-29 22:14:00 +0200

    refactor: Implement shared module to save shared components, pipes, interceptors, ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,17 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ApiAuthenticationInterceptor } from './interceptors/api-authentication.interceptor';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
   ],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: ApiAuthenticationInterceptor,
-    multi: true,
-  }],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
