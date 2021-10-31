d8c468a37e311aaa54d19e8511f807726fcf554f
juaamol <jambulud@gmail.com>
2021-10-31 18:27:02 +0100

    setup translation module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [],
+  exports: [TranslateModule],
 })
 export class SharedModule {}
