644b3218291ab786b3ccac0b5c56172e02c10bf3
juaamol <jambulud@gmail.com>
2021-11-01 13:17:09 +0100

    resolve: #1. Implement genres page

--- src/app/genres/genres.component.spec.ts
+++ src/app/genres/genres.component.spec.ts
@@ -1,6 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 
 import { GenresComponent } from './genres.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable, of } from 'rxjs';
+import { GenresService } from './services/genres/genres.service';
+
+const mockGenres = ['Adventure'];
+
+class MockGenresService {
+  getGenres(): Observable<string[]> {
+    return of(mockGenres);
+  }
+}
 
 describe('GenresComponent', () => {
   let component: GenresComponent;
@@ -8,9 +25,10 @@ describe('GenresComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GenresComponent ]
-    })
-    .compileComponents();
+      declarations: [GenresComponent],
+      imports: [TranslateModule.forRoot(), HttpClientTestingModule],
+      providers: [{ provide: GenresService, useClass: MockGenresService }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +39,35 @@ describe('GenresComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the genres', fakeAsync(() => {
+    component.ngOnInit();
+    tick();
+    expect(component.genres).toEqual(mockGenres);
+  }));
+
+  it('should display the given genres', () => {
+    component.genres = mockGenres;
+    fixture.detectChanges();
+
+    const genreElements = fixture.nativeElement.querySelectorAll('.genre');
+    expect(genreElements.length).toBe(mockGenres.length);
+  });
+
+  it('should display the spinner', () => {
+    component.isLoading = true;
+    fixture.detectChanges();
+
+    const spinnerElements = fixture.nativeElement.querySelectorAll('mat-spinner');
+    expect(spinnerElements.length).toBe(1);
+  });
+
+  it('should display the error', () => {
+    component.hasError = true;
+    fixture.detectChanges();
+
+    const errorElements = fixture.nativeElement.querySelectorAll('.error');
+    expect(errorElements.length).toBe(1);
   });
 });
--- src/app/genres/genres.component.ts
+++ src/app/genres/genres.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GenresService } from './services/genres/genres.service';
 
 @Component({
   selector: 'app-genres',
   templateUrl: './genres.component.html',
-  styleUrls: ['./genres.component.scss']
+  styleUrls: ['./genres.component.scss'],
 })
 export class GenresComponent implements OnInit {
+  isLoading = true;
+  hasError = false;
+  genres: string[] = [];
 
-  constructor() { }
+  constructor(private genresService: GenresService) {}
 
   ngOnInit(): void {
+    this.getGenres().subscribe(
+      (genres) => (this.genres = genres),
+      () => {
+        this.hasError = true;
+        this.isLoading = false;
+        this.genres = [];
+      },
+      () => (this.isLoading = false)
+    );
   }
 
+  private getGenres(): Observable<string[]> {
+    return this.genresService.getGenres();
+  }
 }
