e90ae803f6ecbca3e3a3f2ebb898e7897a236558
juaamol <jambulud@gmail.com>
2021-11-01 11:55:11 +0100

    resolve: #2. Implement MoviesList component

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -1,15 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, from, Observable, of, Subscription } from 'rxjs';
+import { catchError, map, startWith, switchMap, tap } from 'rxjs/operators';
+import { Movie } from './models/movie';
+import { MoviesPage } from './models/movie-page';
+import { MoviesListService } from './services/movies-list/movies-list.service';
 
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
-  styleUrls: ['./movies-list.component.scss']
+  styleUrls: ['./movies-list.component.scss'],
 })
-export class MoviesListComponent implements OnInit {
+export class MoviesListComponent implements OnInit, OnDestroy {
+  displayedColumns: string[] = ['title'];
+  movies: Movie[] = [];
+  resultsLength = 0;
+  genre = '';
+  isLoading = true;
 
-  constructor() { }
+  private moviesPageSubscription: Subscription | undefined;
+  readonly pageSize = 50;
 
-  ngOnInit(): void {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private moviesListService: MoviesListService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.moviesPageSubscription?.unsubscribe();
   }
 
+  ngAfterViewInit() {
+    if (!this.paginator) return;
+
+    this.moviesPageSubscription = this.changePage().subscribe(
+      (movies) => (this.movies = movies)
+    );
+  }
+
+  private changePage(): Observable<Movie[]> {
+    const movieParams = this.getMoviesParams();
+
+    return movieParams.pipe(
+      switchMap((genre) => {
+        this.isLoading = true;
+        return this.getMovies(genre);
+      }),
+      map((movies) => {
+        this.isLoading = false;
+
+        if (movies === null) {
+          return [];
+        }
+
+        this.resultsLength = movies.count;
+        return movies.movies;
+      })
+    );
+  }
+
+  private getMoviesParams(): Observable<string> {
+    const pageUpdate = from(this.paginator.page).pipe(startWith({}));
+
+    return combineLatest([this.genreUpdate(), pageUpdate]).pipe(
+      map(([genre]) => genre)
+    );
+  }
+
+  private genreUpdate(): Observable<string> {
+    return this.route.paramMap.pipe(
+      map((params) => params.get('genre') ?? ''),
+      tap(() => this.paginator.firstPage())
+    );
+  }
+
+  private getMovies(genre: string): Observable<MoviesPage | null> {
+    return this.moviesListService
+      .getMovies(genre, this.paginator.pageIndex + 1)
+      .pipe(catchError(() => of(null)));
+  }
 }
--- src/app/movies-list/movies-list.module.ts
+++ src/app/movies-list/movies-list.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+import { TranslateModule } from '@ngx-translate/core';
+import { ApiAuthenticationInterceptor } from '../shared/interceptors/api-authentication/api-authentication.interceptor';
+import { SharedModule } from '../shared/shared.module';
 import { MoviesListRoutingModule } from './movies-list-routing.module';
 import { MoviesListComponent } from './movies-list.component';
+import { MoviesListService } from './services/movies-list/movies-list.service';
 
 
 @NgModule({
@@ -11,7 +18,24 @@ import { MoviesListComponent } from './movies-list.component';
   ],
   imports: [
     CommonModule,
-    MoviesListRoutingModule
+    SharedModule,
+    HttpClientModule,
+    MoviesListRoutingModule,
+    MatProgressSpinnerModule,
+    SharedModule,
+    MatTableModule,
+    MatPaginatorModule,
+    TranslateModule.forChild({
+      extend: true,
+    }),
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiAuthenticationInterceptor,
+      multi: true,
+    },
+    MoviesListService
   ]
 })
 export class MoviesListModule { }
