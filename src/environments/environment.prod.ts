e7070eee06c2825386bf3a6506c5ae37a76be474
juaamol <jambulud@gmail.com>
2021-11-01 13:40:33 +0100

    add api config to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
   production: true
 };
+
+export const api = {
+  url: 'https://data-imdb1.p.rapidapi.com',
+  key: '59b03f3569mshca7e450f0169fb4p19b17bjsn94584c730c20',
+  host: 'data-imdb1.p.rapidapi.com',
+}
