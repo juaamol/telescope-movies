e0fe55568c74572a2bae378d2407ec579008c67c
juaamol <jambulud@gmail.com>
2021-11-01 13:24:00 +0100

    add toolbar and set default routing to genres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,34 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'genres',
+    pathMatch: 'full',
+  },
+  {
     path: 'single-movie/:movieId',
     loadChildren: () =>
       import('./single-movie/single-movie.module').then(
         (m) => m.SingleMovieModule
       ),
   },
-  { path: 'movies-list/:genre', loadChildren: () => import('./movies-list/movies-list.module').then(m => m.MoviesListModule) },
-  { path: 'genres', loadChildren: () => import('./genres/genres.module').then(m => m.GenresModule) },
+  {
+    path: 'movies-list/:genre',
+    loadChildren: () =>
+      import('./movies-list/movies-list.module').then(
+        (m) => m.MoviesListModule
+      ),
+  },
+  {
+    path: 'genres',
+    loadChildren: () =>
+      import('./genres/genres.module').then((m) => m.GenresModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'genres',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
