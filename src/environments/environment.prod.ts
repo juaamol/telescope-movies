f622007f7f24c48dac6e3fa4c9264eb293a1a9da
juaamol <jambulud@gmail.com>
2021-11-02 11:40:01 +0100

    use api-key file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
+import { API_KEY } from './api-key';
+
 export const environment = {
-  production: true
+  production: true,
 };
 
 export const api = {
   url: 'https://data-imdb1.p.rapidapi.com',
   key: API_KEY,
   host: 'data-imdb1.p.rapidapi.com',
-}
+};
